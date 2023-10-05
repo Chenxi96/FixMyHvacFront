00eb1acd2ad6d2cd04c2c829ffd4908cc3a46361
Chenxi <lin.chenxi14@gmail.com>
2023-10-05 18:45:05 -0400

    cleaned up code in Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,34 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import axios from "axios";
 
 const Profile = () => {
-  const {user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const [item, setItem] = useState()
-
-  useEffect(() => {
-    const getUserMetadata = async () => {
-        try{
-            const accessToken = await getAccessTokenSilently();
-            await axios({
-              url: 'https://fix-my-hvac.onrender.com/getAllImage',
-              method: "GET",
-              headers: {
-                "content-type": "application/json",
-                Authorization: `Bearer ${accessToken}`
-              }
-            }).then(response => setItem(response.data))
-
-        } catch(e) {
-            console.log(e)
-        }
-    };
-    getUserMetadata()
-    return () => {
-      getUserMetadata()
-    }
-  
-  }, [getAccessTokenSilently, setItem]);
+  const {user, isAuthenticated} = useAuth0();
 
   return (
     isAuthenticated && (
