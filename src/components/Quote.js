4fc2d1ca49fe54f0261422574280c197d7c73cc3
Chenxi <lin.chenxi14@gmail.com>
2023-10-17 11:04:10 -0400

    prevent form refresh added redirect

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Quote() {
     const {getAccessTokenSilently} = useAuth0();
+    const navigate = useNavigate();
 
-    
-    const submitChange = async () => {
+
+    const submitChange = async (event) => {
+        event.preventDefault()
         const accessToken = await getAccessTokenSilently();
         const form = document.getElementById('form')
         const formData = new FormData(form)
@@ -15,9 +18,12 @@ export default function Quote() {
                 Authorization: `Bearer ${accessToken}`
             }
         })
-            .then(res => console.log(res))
+        .then(res => console.log(res))
+        .catch(e => console.log(e))
             console.log('from local')
+            navigate('/allQuotes')
     }
+
 
     return (
         <>
@@ -29,9 +35,10 @@ export default function Quote() {
                 <label for="description">Description</label>
                 <textarea type="text" name="description" id="description" ></textarea><br/>
                 <label for="images">Images</label>
-                <input type="file" multiple name="images" id="images"  /><br/>
+                <input type="file" onChange={changeHandle} name="images" id="images" multiple /><br/>
                 <input type="submit" value="Save" />
             </form>
+            {form}
         </>
     )
 }
